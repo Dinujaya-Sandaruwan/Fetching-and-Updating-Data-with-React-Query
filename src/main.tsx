66aa394b0743d706e7265cca6c71de16ec837847
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-05-31 17:56:18 +0530

    9-_Customizing_Query_Settings

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,20 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	// Remote this if it's not needed
+	defaultOptions: {
+		queries: {
+			retry: 3,
+			cacheTime: 300_000, // 5m
+			staleTime: 0, //Data Valide Time
+
+			// refetchOnWindowFocus: false,
+			// refetchOnReconnect: false,
+			// refetchOnMount: false,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React from "react";
 
 interface Todo {
 	id: number;
@@ -18,6 +17,7 @@ function useTodos() {
 	return useQuery<Todo[], Error>({
 		queryKey: ["todos"],
 		queryFn: fetchTodos,
+		staleTime: 10 * 1000,
 	});
 }
 

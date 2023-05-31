cb7b687dda86df2284cf0641125e79412b3b9559
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-05-31 20:15:54 +0530

    11-_Parameterized_Queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -3,22 +3,37 @@ import { useEffect, useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-	const { data: posts, error, isLoading } = usePosts();
+	const [userId, setUserId] = useState<number>();
+	const { data: posts, error, isLoading } = usePosts(userId);
 
 	if (isLoading) return <p>Loading</p>;
 
 	if (error) return <p>{error.message}</p>;
 
 	return (
-		<ul className="list-group">
-			{posts?.map((post) => (
-				<li key={post.id} className="list-group-item">
-					<span style={{ fontWeight: "bold" }}>{post.title}</span>
-					<br />
-					{post.body}
-				</li>
-			))}
-		</ul>
+		<>
+			<select
+				onChange={(event) => setUserId(parseInt(event.target.value))}
+				value={userId}
+				name=""
+				id=""
+				className="form-select mp-3"
+			>
+				<option value=""></option>
+				<option value="1">User 01</option>
+				<option value="2">User 02</option>
+				<option value="3">User 03</option>
+			</select>
+			<ul className="list-group">
+				{posts?.map((post) => (
+					<li key={post.id} className="list-group-item">
+						<span style={{ fontWeight: "bold" }}>{post.title}</span>
+						<br />
+						{post.body}
+					</li>
+				))}
+			</ul>
+		</>
 	);
 };
 
